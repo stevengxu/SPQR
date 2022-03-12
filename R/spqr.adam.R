9db45952f6cb9f641b6abfb77f57b45a42dbdc10
Steven Xu <stevenxu1995@gmail.com>
2022-03-12 17:34:41 -0500

    add MAP layers

--- R/spqr.adam.R
+++ R/spqr.adam.R
@@ -1,6 +1,6 @@
 spqr.adam.train <- 
-  function(params, X, y, seed = NULL, verbose = TRUE)
-{
+  function(params, X, y, seed = NULL, verbose = TRUE) {
+    
   if (is.null(dim(X))) X <- as.matrix(X)
   # Define dataset and dataloader
   quinn_dataset <- dataset(
@@ -37,8 +37,8 @@ spqr.adam.train <-
   if (is.null(params[["model"]])) {
     # Use the default template
     
-    DeepQuinn <- nn_module(
-      classname = NULL,
+    nn_spqr <- nn_module(
+      classname = "nn_spqr",
       initialize = function(varnum, n.hidden = c(8,8), n.knots, 
                             dropout = c(0,0), batchnorm = F, 
                             activation = "tanh") {
@@ -61,23 +61,23 @@ spqr.adam.train <-
           }
         }
         
-        self$layernum <- length(n.hidden)
+        self$layernum <- length(n.hidden)+1
         self$fc <- nn_module_list()
         
         # Input Layer
         self$fc[[1]] <- mlp(varnum, n.hidden[1], dropout[1])
         
-        if (self$layernum > 1) {
+        if (self$layernum > 2) {
           # Hidden Layers
-          for (i in 2:self$layernum) {
+          for (i in 2:(self$layernum-1)) {
             self$fc[[i]] <- 
               mlp(n.hidden[i-1], n.hidden[i], dropout[2])
           }
         }
-    
+        
         # Output Layer
-        self$output <- nn_sequential(
-          nn_linear(n.hidden[self$layernum], n.knots),
+        self$fc[[self$layernum]] <- nn_sequential(
+          nn_linear(n.hidden[self$layernum-1], n.knots),
           nn_softmax(2)
         )
         
@@ -85,11 +85,11 @@ spqr.adam.train <-
       
       forward = function(x) {
         for (i in 1:self$layernum) { x <- self$fc[[i]](x) }
-        x %>% self$output()
+        x
       }
     )
     
-    model <- DeepQuinn(ncol(X), params[["n.hidden"]], params[["n.knots"]], 
+    model <- nn_spqr(ncol(X), params[["n.hidden"]], params[["n.knots"]], 
                        params[["dropout"]], params[["batchnorm"]], 
                        params[["activation"]])
     for (i in 1:model$layernum)
@@ -107,17 +107,10 @@ spqr.adam.train <-
   
   # Define custom loss function
   nloglik_loss = function(observed, predicted, indices) {
-    Bobs <- observed[indices] #Subset the basis 
-    sumprod <- torch_sum(torch_mul(predicted,Bobs),2)
-    logscore <- torch_sum(-torch_log(sumprod))
-    return(logscore)
-  }
-  
-  discrepency_loss = function(observed,predicted,indices) {
-    Bobs <- observed[indices] #Subset the basis 
-    sumprod <- torch_sum(torch_mul(predicted,Bobs),2)
-    sumlik <- 2 * torch_mean(sumprod)
-    
+    B <- observed[indices] #Subset the basis 
+    probs <- B$mul(predicted)$sum(2)
+    loglik <- probs$log()$sum()
+    return(-loglik)
   }
   
   optimizer <- optim_adam(model$parameters, lr = params[["lr"]])
@@ -125,7 +118,7 @@ spqr.adam.train <-
   counter <- 0
   if(!dir.exists(params[["save.path"]]))
     dir.create(params[["save.path"]])
-    
+  
   save_name <- file.path(params[["save.path"]], params[["save.name"]])
   for (epoch in 1:params[["epochs"]]) {
     
@@ -156,6 +149,7 @@ spqr.adam.train <-
       indices <- b$index
       loss <- nloglik_loss(observed = Bvalid_tensor, predicted = output, 
                            indices = indices)
+      
       valid_losses <- c(valid_losses, loss$item())
       
     })
@@ -179,3 +173,115 @@ spqr.adam.train <-
               n.knots = n.knots)
   return(out)
 }
+
+
+nn_BayesLinear_GP <- nn_module(
+  classname = "nn_BayesLinear",
+  initialize = function(in_features, out_features, tau_prior_a, tau_prior_b, scale_by_width = FALSE) {
+    
+    self$W <- nn_parameter(nn_init_xavier_uniform_(torch_zeros(out_features,in_features)))
+    # precision hyperparameter for W
+    self$tau_W <- nn_parameter(torch_tensor(1))
+    
+    self$b <- nn_parameter(nn_init_xavier_uniform_(torch_zeros(out_features,1)))
+    # precision hyperparameter for b
+    self$tau_b <- nn_parameter(torch_tensor(1))
+    
+    # shape and rate hyperparameters for prior of tau_W and tau_b
+    self$tpa <- torch_tensor(tau_prior_a)
+    self$tpb <- torch_tensor(tau_prior_b)
+    
+    if (scale_by_width) {
+      self$H <- torch_tensor(in_features)
+    } else {
+      self$H <- torch_tensor(1)
+    }
+  },
+  
+  forward = function(input) {
+    output <- torch_mm(self$W,input)$add(self$b)
+    sig_W <- torch_divide(1,self$tau_W$sqrt())
+    sig_b <- torch_divide(1,self$tau_b$sqrt())
+    logprior <- torch_tensor(0)$sum()
+    logprior$add_(distr_normal(0,sig_W)$log_prob(self$W)$sum())
+    logprior$add_(distr_gamma(self$tpa,self$tpb$divide(self$H))$log_prob(self$tau_W)$sum())
+    logprior$add_(distr_normal(0,sig_b)$log_prob(self$b)$sum())
+    logprior$add_(distr_gamma(self$tpa,self$tpb)$log_prob(self$tau_b)$sum())
+    return(list(output=output, logprior=logprior))
+  }
+)
+
+nn_BayesLinear_ARD <- nn_module(
+  classname = "nn_BayesLinear",
+  initialize = function(in_features, out_features, tau_prior_a, tau_prior_b) {
+    
+    self$W <- nn_parameter(nn_init_xavier_uniform_(torch_zeros(out_features,in_features)))
+    # precision hyperparameter for W
+    self$tau_W <- nn_parameter(torch_tensor(in_features))
+    
+    self$b <- nn_parameter(nn_init_xavier_uniform_(torch_zeros(out_features,1)))
+    # precision hyperparameter for b
+    self$tau_b <- nn_parameter(torch_tensor(1))
+    
+    # shape and rate hyperparameters for prior of tau_W and tau_b
+    self$tpa <- torch_tensor(sigma_prior_a)
+    self$tpb <- torch_tensor(sigma_prior_b)
+    
+  },
+  
+  forward = function(input) {
+    output <- torch_mm(self$W,input)$add(self$b)
+    sig_W <- torch_divide(1,self$tau_W$sqrt())
+    sig_b <- torch_divide(1,self$tau_b$sqrt())
+    logprior <- torch_tensor(0)$sum()
+    for (i in 1:self$W$size[2]) {
+      logprior$add_(distr_normal(0,sig_W[i])$log_prob(self$W[,i])$sum())
+      logprior$add_(distr_gamma(self$tpa,self$tpb)$log_prob(self$tau_W[i])$sum())
+    }
+    logprior$add_(distr_normal(0,sig_b)$log_prob(self$b)$sum())
+    logprior$add_(distr_gamma(self$tpa,self$tpb)$log_prob(self$tau_b)$sum())
+    return(list(output=output, logprior=logprior))
+  }
+)
+
+nn_BayesLinear_GSM <- nn_module(
+  classname = "nn_BayesLinear",
+  initialize = function(in_features, out_features, tau_prior_a, tau_prior_b,
+                        kappa_prior_a, kappa_prior_b) {
+    
+    # global precision hyperparameter
+    self$tau <- nn_parameter(torch_tensor(1))
+    
+    self$W <- nn_parameter(nn_init_xavier_uniform_(torch_zeros(out_features,in_features)))
+    # local precision hyperparameter for W
+    self$kappa_W <- nn_parameter(torch_tensor(in_features))
+    
+    self$b <- nn_parameter(nn_init_xavier_uniform_(torch_zeros(out_features,1)))
+    # local precision hyperparameter for b
+    self$kappa_b <- nn_parameter(torch_tensor(1))
+    
+    # shape and rate hyperparameters for prior of tau
+    self$tpa <- torch_tensor(tau_prior_a)
+    self$tpb <- torch_tensor(tau_prior_b)
+    # shape and rate hyperparameters for prior of kappa
+    self$kpa <- torch_tensor(kappa_prior_a)
+    self$kpb <- torch_tensor(kappa_prior_b)
+    
+  },
+  
+  forward = function(input) {
+    output <- torch_mm(self$W,input)$add(self$b)
+    sig <- torch_divide(1,self$tau$sqrt())
+    lambda_W <- torch_divide(1,self$kappa_W$sqrt())
+    lambda_b <- torch_divide(1,self$kappa_b$sqrt())
+    logprior <- torch_tensor(0)$sum()
+    for (i in 1:self$W$size[2]) {
+      logprior$add_(distr_normal(0,torch_mul(sig,lambda_W[i]))$log_prob(self$W[,i])$sum())
+      logprior$add_(distr_gamma(self$kpa,self$kpb)$log_prob(self$kappa_W[i])$sum())
+    }
+    logprior$add_(distr_normal(0,torch_mul(sig,lambda_b))$log_prob(self$b)$sum())
+    logprior$add_(distr_gamma(self$kpa,self$kpb)$log_prob(self$kappa_b)$sum())
+    logprior$add_(distr_gamma(self$tpa,self$tpb)$log_prob(self$tau)$sum())
+    return(list(output=output, logprior=logprior))
+  }
+)
